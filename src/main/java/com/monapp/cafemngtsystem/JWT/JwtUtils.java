01579d51ec67f45c756e88ccc81b90046a1caa60
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-08 15:43:48 +0000

    commit project

--- src/main/java/com/monapp/cafemngtsystem/JWT/JwtUtils.java
+++ src/main/java/com/monapp/cafemngtsystem/JWT/JwtUtils.java
@@ -2,10 +2,12 @@ package com.monapp.cafemngtsystem.JWT;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -31,6 +33,14 @@ public class JwtUtils {
         return extractExpiration(token).before(new Date());
     }
 
+    private String createToken(Map<String,Object> claims, String subject){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256,secret).compact();
+    }
     public Boolean validateToken(String token , UserDetails userDetails){
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
