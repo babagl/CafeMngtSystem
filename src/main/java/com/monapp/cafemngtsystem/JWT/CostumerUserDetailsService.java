c266ee61b94a6dd90a78f442e126128e47834bd6
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-09 19:39:00 +0000

    commit project

--- src/main/java/com/monapp/cafemngtsystem/JWT/CostumerUserDetailsService.java
+++ src/main/java/com/monapp/cafemngtsystem/JWT/CostumerUserDetailsService.java
@@ -1,4 +1,18 @@
 package com.monapp.cafemngtsystem.JWT;
 
-public class CostumerUserDetailsService {
+import com.monapp.cafemngtsystem.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CostumerUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
