0d17435793e6d40b327a681ec8d763825d0b9ebf
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-09 20:56:19 +0000

    commit project

--- src/main/java/com/monapp/cafemngtsystem/JWT/CostumerUserDetailsService.java
+++ src/main/java/com/monapp/cafemngtsystem/JWT/CostumerUserDetailsService.java
@@ -1,18 +1,36 @@
 package com.monapp.cafemngtsystem.JWT;
 
+
 import com.monapp.cafemngtsystem.dao.UserDao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Objects;
+@Slf4j
+@Service
 public class CostumerUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserDao userDao;
-
+    private com.monapp.cafemngtsystem.POJO.User userDetail;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("inside loadUserByUsername{}", email);
+        userDetail =userDao.findByEmailId(email);
+        if (!Objects.isNull(userDetail)){
+            return new User(userDetail.getEmail(), userDetail.getPassword(),new ArrayList<>());
+        }else {
+            throw new UsernameNotFoundException("User "+email+" not found");
+        }
     }
+    public com.monapp.cafemngtsystem.POJO.User getUserDetail(){
+        return userDetail;
+    }
+
 }
