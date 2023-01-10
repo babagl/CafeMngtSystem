2fc317c035bcdb2c1e0a88a0eda7b14a6b197601
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-10 15:04:40 +0000

    commit project

---
+++ src/main/java/com/monapp/cafemngtsystem/JWT/JwtFilter.java
@@ -0,0 +1,31 @@
+package com.monapp.cafemngtsystem.JWT;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Service
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private CostumerUserDetailsService service;
+    Claims claims = null;
+    private String Username = null;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getServletPath().matches("user/login|/user/forgotPassword|/user/signup")){
+            filterChain.doFilter(request,response);
+        }else {
+
+        }
+    }
+}
--- src/main/java/com/monapp/cafemngtsystem/JWT/SecurityConfig.java
+++ src/main/java/com/monapp/cafemngtsystem/JWT/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.monapp.cafemngtsystem.JWT;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,14 +9,22 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private CostumerUserDetailsService costumerUserDetailsService;
+
+
+    @Autowired
+    CostumerUserDetailsService costumerUserDetailsService;
+    @Autowired
+     JwtFilter jwtFilter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(costumerUserDetailsService);
@@ -36,6 +45,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
                 .and()
                 .csrf().disable()
-                .antMatcher("/user/login","/user/signup","user/forgotPassword");
+                .authorizeRequests()
+                .antMatchers("/user/login","/user/signup","user/forgotPassword")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore("",UsernamePasswordAuthenticationFilter.class);
     }
 }
