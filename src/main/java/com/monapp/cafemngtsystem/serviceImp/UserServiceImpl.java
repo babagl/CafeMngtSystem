c72e73d3c3c9751ebcf3352833068350de215a8c
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-01-11 23:52:56 +0000

    commit project

--- src/main/java/com/monapp/cafemngtsystem/serviceImp/UserServiceImpl.java
+++ src/main/java/com/monapp/cafemngtsystem/serviceImp/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.monapp.cafemngtsystem.serviceImp;
 
+import com.monapp.cafemngtsystem.JWT.CostumerUserDetailsService;
+import com.monapp.cafemngtsystem.JWT.JwtUtils;
 import com.monapp.cafemngtsystem.POJO.User;
 import com.monapp.cafemngtsystem.constents.CafeConstants;
 import com.monapp.cafemngtsystem.dao.UserDao;
@@ -9,6 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -19,6 +24,12 @@ import java.util.Objects;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    CostumerUserDetailsService costumerUserDetailsService;
+    @Autowired
+    JwtUtils jwtUtils;
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         try {
@@ -38,6 +49,26 @@ public class UserServiceImpl implements UserService {
             ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> login(Map<String, String> requestMap) {
+        log.info("inside login");
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(requestMap.get("email"),requestMap.get("password"))
+            );
+            if (auth.isAuthenticated()){
+                if (costumerUserDetailsService.getUserDetail().getStatus().equalsIgnoreCase("true")){
+                    return new ResponseEntity<String>("{\"token\":\""+
+                            jwtUtils.generatedToken(costumerUserDetailsService.getUserDetail().getEmail(),
+                                    costumerUserDetailsService.getUserDetail().getRoles()) +"\"}" ,HttpStatus.OK);
+                }
+            }
+        }catch (Exception ex){
+            log.error("{}",ex);
+        }
+        return null;
     }
 
     private boolean validateSignUpMap(Map<String, String> requestMap){
